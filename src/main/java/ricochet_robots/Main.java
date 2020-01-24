5b9857eb4948655f45e45d9b1538add2d8951bf3
Lorada ANDRÉ <dada.andre2@gmail.com>
2020-01-24 20:36:59 +0100

    chore: Configuration de Gradle

---
+++ src/main/java/ricochet_robots/Main.java
@@ -0,0 +1,11 @@
+package ricochet_robots;
+
+public class Main {
+    public String getGreeting() {
+        return "Nope hello world.";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Main().getGreeting());
+    }
+}
