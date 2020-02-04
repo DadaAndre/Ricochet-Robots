b712b736801b28237f7f168ff82792fc246b0cc2
Kachri Oussama <oussamakachri@gmail.com>
2020-02-04 15:37:50 +0100

    Feat: Ajout des methodes : intToBinary - stringToMiniGrille - creerMiniGrille

--- src/ricochet_robots/Main.java
+++ src/ricochet_robots/Main.java
@@ -4,5 +4,7 @@ public class Main{
 
     public static void main(String[] args) {
         System.out.println("I'ts okay");
+        Plateau plat = new Plateau();
+        System.out.println(plat.creerMiniGrille("0,1,2,5,4"));
     }
 }
--- src/ricochet_robots/Plateau.java
+++ src/ricochet_robots/Plateau.java
@@ -1,6 +1,42 @@
 package ricochet_robots;
 
+import static java.lang.Integer.toBinaryString;
+import java.util.Arrays;
+
 public class Plateau{
 
- 
+    public Plateau(){}
+
+        //Methode qui convertit un entier a un nombre binaire 
+
+        public String intTobinary(int number) {
+            String str = toBinaryString(number);
+            while(str.length()<4){
+                str="0"+str;
+            }
+            return "["+str.charAt(0)+","+str.charAt(1)+","+str.charAt(2)+","+str.charAt(3)+"]";
+        }
+
+        //Methode qui transforme une chaine de caractere en une mini-grille
+
+        public String[]  stringToMiniGrille(String chaine){
+            String [] tab1D = chaine.split(",");
+            
+            for (int i=0 ; i<tab1D.length;i++) {
+                int num = Integer.parseInt(tab1D[i]);
+                tab1D[i] = intTobinary(num);
+            }
+            return tab1D;
+        }
+
+        //Methode pour creer une mini-grille
+
+        public String  creerMiniGrille(String chaine){
+            String[] str =stringToMiniGrille(chaine);
+            String str2 = Arrays.toString(str);
+            return str2;
+        }
+
+
 }
+  
