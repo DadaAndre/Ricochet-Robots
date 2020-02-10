eb169b6717d7023b9f2af41a5b93947ccecffd19
Auvray Theo <21707781@C304L-138C08.campus.unicaen.fr>
2020-02-10 23:40:47 +0100

    feat : Initialisation de  la classe jeton

---
+++ src/ricochet_robots/Deplacement.java
@@ -0,0 +1,8 @@
+package ricochet_robots;
+
+public class Deplacement{
+	private static final int directionhaut = 1;
+	private static final int directiondroite = 2;
+	private static final int directionbas = 3;
+	private static final int directiongauche = 4;
+}
