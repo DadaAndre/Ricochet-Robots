1821d0171f0003e772848a8867b980245e217296
Lorada ANDRÉ <dada.andre2@gmail.com>
2020-02-18 16:34:50 +0100

    feat: Ajout de la collision entre les robots

--- src/ricochet_robots/Robot.java
+++ src/ricochet_robots/Robot.java
@@ -52,8 +52,52 @@ public class Robot{
 		this.positionY = positionY;
 	}
 
-	public String couleur(){
+	public String getCouleur(){
 		return couleur;
+	}
+
+	public void estUneCollisionRobot(direction){
+		switch(direction){
+			case DIRECTION_HAUT:
+				for(int i = 0: i< plateau.tableauRobots.length; i++){
+					if(plateau.tableauRobots.getCouleur() != couleur && plateau.tableauRobots.getPositionX() == positionX &&  plateau.tableauRobots.getPositionY() == (positionY - 1)){
+						return true;
+					}
+				}
+				return false;
+
+				break;
+
+			case DIRECTION_BAS:
+				for(int i = 0: i< plateau.tableauRobots.length; i++){
+					if(plateau.tableauRobots.getCouleur() != couleur && plateau.tableauRobots.getPositionX() == positionX &&  plateau.tableauRobots.getPositionY() == (positionY + 1)){
+						return true;
+					}
+				}
+				return false;
+
+				break;
+
+			case DIRECTION_GAUCHE:
+				for(int i = 0: i< plateau.tableauRobots.length; i++){
+					if(plateau.tableauRobots.getCouleur() != couleur && plateau.tableauRobots.getPositionX() == (positionX - 1)  &&  plateau.tableauRobots.getPositionY() == positionY){
+						return true;
+					}
+				}
+				return false;
+
+				break;
+
+			case DIRECTION_DROITE:
+				for(int i = 0: i< plateau.tableauRobots.length; i++){
+					if(plateau.tableauRobots.getCouleur() != couleur && plateau.tableauRobots.getPositionX() == (positionX + 1)  &&  plateau.tableauRobots.getPositionY() == positionY){
+						return true;
+					}
+				}
+				return false;
+
+				break;
+		}
 	}
 
 	//Déplacement du robot
@@ -61,22 +105,22 @@ public class Robot{
 		//Vérification de la direction choisie
 		if(direction == Deplacement.DIRECTION_HAUT){
 			//Tant que le robot ne rencontre pas un mur en haut, il se dirige vers le haut
-			while(this.plateau.getCase(positionX, positionY - 1).getValHaut() != 1){
+			while(this.plateau.getCase(positionX, positionY - 1).getValHaut() != 1 && !estUneCollisionRobot()){
 				this.positionY -= 1;
 			}
 		}else if(direction == Deplacement.DIRECTION_BAS){
 			//Tant que le robot ne rencontre pas un mur en bas, il se dirige vers le bas
-			while(this.plateau.getCase(positionX, positionY + 1).getValBas() != 1){
+			while(this.plateau.getCase(positionX, positionY + 1).getValBas() != 1 && !estUneCollisionRobot()){
 				this.positionY += 1;
 			}
 		}else if(direction == Deplacement.DIRECTION_GAUCHE){
 			//Tant que le robot ne rencontre pas un mur à gauche, il se dirige vers la gauche
-			while(this.plateau.getCase(positionX - 1, positionY).getValGauche() != 1){
+			while(this.plateau.getCase(positionX - 1, positionY).getValGauche() != 1 && !estUneCollisionRobot()){
 				this.positionX -= 1;
 			}
 		}else if(direction == Deplacement.DIRECTION_DROITE){
 			//Tant que le robot ne rencontre pas un mur à droite, il se dirige vers la droite
-			while(this.plateau.getCase(positionX + 1, positionY).getValDroit() != 1){
+			while(this.plateau.getCase(positionX + 1, positionY).getValDroit() != 1 && !estUneCollisionRobot()){
 				this.positionX += 1;
 			}
 		}
